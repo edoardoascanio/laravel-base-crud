e86a3e2b9dc7b0c8a3e0f48f79860d78aae98212
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-07-08 15:37:36 +0200

    mod. index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -8,7 +8,7 @@
 
 @section('content')
 
-    <a href="{{ route('user.create')}}">Aggiungi comic</a>
+    <a href="{{ route('comics.create')}}">Aggiungi comic</a>
 
     @foreach($comics as $comic){
 
@@ -17,9 +17,9 @@
                 <li><h1>{{ $comic->title }}</h1></li>
                 <li>{{ $comic->type}}</li>
                 <li>
-                    <a href="{{route ('comics.show', $user->id )}}">Mostra dettagli</a>
+                    <a href="{{route ('comics.show', $comic->id )}}">Mostra dettagli</a>
               
-                    <a href="{{route ('comics.edit', $user->id )}}">Modifica</a>
+                    <a href="{{route ('comics.edit', $comic->id )}}">Modifica</a>
                 </li>
 
                 @include('components.deleteBtn',["id" => $comic->id])
